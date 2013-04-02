7780c9bfa73a473912147ef47d6573132dc66d3c
Ruslan Diachenko <rd.ryly@gmail.com>
2013-04-02 18:04:22 +0300

    Issue #5: Create a simple server for logging events

--- jlv/src/main/java/com/rdiachenko/jlv/log4j/socketappender/ClientThread.java
+++ jlv/src/main/java/com/rdiachenko/jlv/log4j/socketappender/ClientThread.java
@@ -1,6 +1,8 @@
 package com.rdiachenko.jlv.log4j.socketappender;
 
 import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -9,7 +11,7 @@ import org.apache.log4j.spi.LoggingEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.rdiachenko.jlv.log4j.domain.LogContainer;
+import com.rdiachenko.jlv.io.TextFileSaver;
 
 public class ClientThread implements Runnable {
 
@@ -17,25 +19,28 @@ public class ClientThread implements Runnable {
 	
 	private Socket socket = null;
 	
-	private LogContainer container = null;
-
 	public ClientThread(Socket socket) {
 		this.socket = socket;
-		container = LogContainer.createNewContainer();
 	}
 
 	public void run() {
+		File file = new File("src/main/resources/log.log");
+		TextFileSaver fileSaver = null;
 		ObjectInputStream inputStream = null;
 
 		try {
+			fileSaver = new TextFileSaver(file);
 			inputStream = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
 			Object object = null;
 
 			while ((object = inputStream.readObject()) != null) {
 				LoggingEvent log = (LoggingEvent) object;
-				container.add(log);
+				fileSaver.sendTextToFile(logAsString(log));
 			}
 
+		} catch (FileNotFoundException e) {
+			logger.error("Errors occur while creating|modifying file: " + file.getName(), e);
+			
 		} catch (IOException e) {
 			logger.error("Errors occur while reading from socket's input stream: ", e);
 
@@ -43,6 +48,9 @@ public class ClientThread implements Runnable {
 			logger.error("Couldn't recognized LoggingEvent object from the socket's input stream: ", e);
 
 		} finally {
+			if (fileSaver != null) {
+				fileSaver.flushAndClose();
+			}
 			try {
 				inputStream.close();
 			} catch (IOException e) {
@@ -54,5 +62,14 @@ public class ClientThread implements Runnable {
 				logger.error("Socket could not be closed: ", e);
 			}
 		}
+	}
+	
+	private String logAsString(LoggingEvent le) {
+		StringBuilder log = new StringBuilder();
+		
+		if (le != null) {
+			log.append(le.getMessage());
+		}
+		return log.toString();
 	}
 }
