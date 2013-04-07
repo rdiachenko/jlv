fe847adf2ab0e8b0048fba1efc9a8c98b6e9b180
Ruslan Diachenko <rd.ryly@gmail.com>
2013-04-08 00:36:02 +0300

    Issue #6: Create server settings page

---
+++ jlv-eclipse-plugin/jlv-plugin/src/com/rdiachenko/jlv/ui/preferences/GeneralPreferencePage.java
@@ -0,0 +1,21 @@
+package com.rdiachenko.jlv.ui.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class GeneralPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
