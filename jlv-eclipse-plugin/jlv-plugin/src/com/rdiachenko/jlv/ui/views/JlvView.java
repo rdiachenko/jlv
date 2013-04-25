84a80711f5dfff6083988aada8e171f1fcea5707
Ruslan Diachenko <rd.ryly@gmail.com>
2013-04-25 18:55:25 +0300

    Issue #7: Create simple logging view

---
+++ jlv-eclipse-plugin/jlv-plugin/src/com/rdiachenko/jlv/ui/views/JlvView.java
@@ -0,0 +1,24 @@
+package com.rdiachenko.jlv.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class JlvView extends ViewPart {
+
+	public JlvView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jlv-eclipse-plugin/jlv-plugin/src/com/rdiachenko/jlv/ui/views/StopServerAction.java
@@ -0,0 +1,28 @@
+package com.rdiachenko.jlv.ui.views;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+public class StopServerAction implements IViewActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IViewPart view) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
