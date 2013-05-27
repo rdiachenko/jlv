0c893abf7f58b9a90fb3b3b608e57a70eb345e4a
Ruslan Diachenko <rd.ryly@gmail.com>
2013-05-27 19:58:28 +0300

    Issue #16: Write unit tests for logging server and client

--- jlv/src/main/java/com/rdiachenko/jlv/log4j/dao/LogsInsTrigger.java
+++ jlv/src/main/java/com/rdiachenko/jlv/log4j/dao/LogsInsTrigger.java
@@ -5,14 +5,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.h2.tools.TriggerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.rdiachenko.jlv.log4j.domain.Log;
 import com.rdiachenko.jlv.log4j.domain.LogEventContainer;
 
 public class LogsInsTrigger extends TriggerAdapter {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Override
 	public void fire(Connection conn, ResultSet oldRow, ResultSet newRow) throws SQLException {
+		logger.info("Triggering new log...");
 		if (newRow.next()) {
 			Log log = (new Log.Builder()).categoryName(newRow.getString("category"))
 					.className(newRow.getString("class"))
@@ -27,6 +32,21 @@ public class LogsInsTrigger extends TriggerAdapter {
 					.message(newRow.getString("message"))
 					.build();
 			LogEventContainer.notifyListeners(log);
+			logger.debug("Trigger new log: {}", log);
 		}
+	}
+
+	@Override
+	public void close() throws SQLException {
+		logger.info("Closing database...");
+		super.close();
+		logger.info("The database is closed");
+	}
+
+	@Override
+	public void remove() throws SQLException {
+		logger.info("Dropping {} trigger...", getClass());
+		super.remove();
+		logger.info("The trigger was dropped");
 	}
 }
--- jlv/src/test/java/com/rdiachenko/jlv/log4j/socketappender/ClientThreadTest.java
+++ jlv/src/test/java/com/rdiachenko/jlv/log4j/socketappender/ClientThreadTest.java
@@ -1,12 +1,14 @@
 package com.rdiachenko.jlv.log4j.socketappender;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -15,77 +17,66 @@ import org.slf4j.LoggerFactory;
 import com.rdiachenko.jlv.log4j.dao.LogDao;
 import com.rdiachenko.jlv.log4j.dao.LogDaoImpl;
 import com.rdiachenko.jlv.log4j.domain.Log;
-import com.rdiachenko.jlv.log4j.domain.LogEventContainer;
-import com.rdiachenko.jlv.log4j.domain.LogEventListener;
 
 public class ClientThreadTest {
 
 	private LogDao logDao;
-
-	private ClientApp client;
 
 	private ServerRunner serverRunner;
 
 	private ExecutorService executor;
 
 	@Test
-	public void testClient() {
-		String[] expectedLogMessages = {
+	public void testClient() throws InterruptedException {
+		Logger logger = LoggerFactory.getLogger(getClass());
+		PropertyConfigurator.configure("src/test/resources/log4j-test.properties");
+
+		final String[] expectedLogMessages = {
 				"debug test message 1",
 				"info test message 1",
 				"error test message 1",
 		};
-		final String[] actualLogMessages = new String[expectedLogMessages.length];
 
-		LogEventListener listener = new LogEventListener() {
-			private int index = 0;
+		logger.debug(expectedLogMessages[0]);
+		logger.info(expectedLogMessages[1]);
+		logger.error(expectedLogMessages[2]);
 
-			public void handleLogEvent(Log log) {
-				actualLogMessages[index] = log.getMessage();
-				index++;
-			}
-		};
+		// Reset configuration in order the only the above three logs are inserted to db
+		PropertyConfigurator.configure("src/main/resources/log4j.properties");
 
-		LogEventContainer.addListener(listener);
+		// Wait for logs
+		Thread.sleep(2000);
+		Iterator<Log> logs = logDao.getTailingLogs(3).iterator();
 
-		client.logger.debug(expectedLogMessages[0]);
-		client.logger.info(expectedLogMessages[1]);
-		client.logger.error(expectedLogMessages[2]);
+		Assert.assertTrue(logs.hasNext());
+		Assert.assertEquals(expectedLogMessages[2], logs.next().getMessage());
 
-		LogEventContainer.removeListener(listener);
+		Assert.assertTrue(logs.hasNext());
+		Assert.assertEquals(expectedLogMessages[1], logs.next().getMessage());
 
-//		Assert.assertArrayEquals(expectedLogMessages, actualLogMessages);
+		Assert.assertTrue(logs.hasNext());
+		Assert.assertEquals(expectedLogMessages[0], logs.next().getMessage());
+
+		Assert.assertFalse(logs.hasNext());
 	}
 
 	@Before
-	public void initServerAndClient() throws IOException {
+	public void init() throws IOException {
 		logDao = new LogDaoImpl();
 		logDao.initDb();
-
 		executor = Executors.newSingleThreadExecutor();
 		serverRunner = new ServerRunner();
 		executor.execute(serverRunner);
-		client = new ClientApp();
 	}
 
 	@After
-	public void stopServerAndClient() throws IOException, InterruptedException {
-		logDao.dropDb();
+	public void stop() throws IOException, InterruptedException {
 		serverRunner.stop();
 		executor.shutdown();
-
-		if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+		if (!executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
 			executor.shutdownNow();
 		}
-	}
-
-	private final static class ClientApp {
-
-		private final Logger logger = LoggerFactory.getLogger(getClass());
-
-		private ClientApp() {
-			PropertyConfigurator.configure("src/test/resources/log4j-test.properties");
-		}
+		logDao.dropDb();
 	}
 
 	private final static class ServerRunner implements Runnable {
