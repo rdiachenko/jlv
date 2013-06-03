8f6deb17cd6dd5687b6fdbacf86a780423ba46f4
Ruslan Diachenko <rd.ryly@gmail.com>
2013-06-03 16:07:54 +0300

    Issue #22: Improve deployment process

---
+++ build-scripts/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo '[JLV BUILD]'
+cd ../jlv && mvn clean install
+status=$?
+
+if [ "$status" -gt 0 ]
+then
+    echo '[BUILD FAILED]'
+    exit $status
+fi
+
+echo '[JLV-PLUGIN BUILD]'
+cd ../jlv-eclipse-plugin/jlv-plugin && mvn clean install -DskipTests
+status=$?
+
+if [ "$status" -gt 0 ]
+then
+    echo '[BUILD FAILED]'
+else
+    echo '[BUILD SUCCESS]'
+fi
+
+exit $status
