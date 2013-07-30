4af90730f34677a3a9af30dd2d4b9bad9a61f54b
Ruslan Diachenko <rd.ryly@gmail.com>
2013-07-31 00:58:33 +0300

    Issue #23, #34: limitation ranges for buffer size and refreshing time were added

--- jlv-eclipse-plugin/jlv-plugin/src/com/rdiachenko/jlv/ui/preferences/GeneralPreferencePage.java
+++ jlv-eclipse-plugin/jlv-plugin/src/com/rdiachenko/jlv/ui/preferences/GeneralPreferencePage.java
@@ -10,6 +10,12 @@ import com.rdiachenko.jlv.JlvActivator;
 
 public class GeneralPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
+	private static final int LOG_VIEW_BUFFER_SIZE_MIN = 50;
+	private static final int LOG_VIEW_BUFFER_SIZE_MAX = 100000;
+
+	private static final int LOG_VIEW_REFRESHING_TIME_MIN = 1000;
+	private static final int LOG_VIEW_REFRESHING_TIME_MAX = 60000;
+
 	public GeneralPreferencePage() {
 		super(GRID);
 		setPreferenceStore(JlvActivator.getDefault().getPreferenceStore());
@@ -36,11 +42,13 @@ public class GeneralPreferencePage extends FieldEditorPreferencePage implements 
 		IntegerFieldEditor logViewBufferSizePrefEditor = new IntegerFieldEditor(
 				PreferenceManager.JLV_LOGVIEW_BUFFER_SIZE,
 				"Buffer size (logs):", getFieldEditorParent());
+		logViewBufferSizePrefEditor.setValidRange(LOG_VIEW_BUFFER_SIZE_MIN, LOG_VIEW_BUFFER_SIZE_MAX);
 		addField(logViewBufferSizePrefEditor);
 
 		IntegerFieldEditor logViewRefreshingTimePrefEditor = new IntegerFieldEditor(
 				PreferenceManager.JLV_LOGVIEW_REFRESHING_TIME,
 				"Refreshing time (ms):", getFieldEditorParent());
+		logViewRefreshingTimePrefEditor.setValidRange(LOG_VIEW_REFRESHING_TIME_MIN, LOG_VIEW_REFRESHING_TIME_MAX);
 		addField(logViewRefreshingTimePrefEditor);
 	}
 }
