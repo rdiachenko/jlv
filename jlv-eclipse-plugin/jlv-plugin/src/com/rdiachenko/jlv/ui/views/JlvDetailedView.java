8a98ee1c7aa7e694f17abcfba41b0dd72674f027
Ruslan Diachenko <rd.ryly@gmail.com>
2013-08-13 00:41:06 +0300

    Detailed jlv view skeleton was created

---
+++ jlv-eclipse-plugin/jlv-plugin/src/com/rdiachenko/jlv/ui/views/JlvDetailedView.java
@@ -0,0 +1,24 @@
+package com.rdiachenko.jlv.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class JlvDetailedView extends ViewPart {
+
+	public JlvDetailedView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
