094e626e1d7a069455acab1cb842949cceca0563
Ruslan Diachenko <rd.ryly@gmail.com>
2013-08-20 17:14:59 +0300

    dao provider was created

---
+++ jlv/src/main/java/com/rdiachenko/jlv/log4j/dao/DaoProvider.java
@@ -0,0 +1,16 @@
+package com.rdiachenko.jlv.log4j.dao;
+
+public enum DaoProvider {
+
+	LOG_DAO(new LogDaoImpl());
+
+	private LogDao logDao;
+
+	private DaoProvider(LogDao logDao) {
+		this.logDao = logDao;
+	}
+
+	public LogDao getLogDao() {
+		return logDao;
+	}
+}
