df720f33de0e42da37c9ba95671b24e4087aee06
Ruslan Diachenko <rd.ryly@gmail.com>
2015-03-09 22:16:45 +0200

    spinner field editor was implemented

---
+++ jlv-eclipse-plugin/jlv-plugin/src/com/github/rd/jlv/eclipse/ui/preferences/SpinnerFieldEditor.java
@@ -0,0 +1,69 @@
+package com.github.rd.jlv.eclipse.ui.preferences;
+
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Spinner;
+
+import com.github.rd.jlv.props.EventScope;
+import com.github.rd.jlv.props.PropertyKey;
+
+public class SpinnerFieldEditor extends FieldEditor {
+
+	private Spinner field;
+	private String label;
+	private int value;
+	private PropertyKey key;
+
+	public SpinnerFieldEditor(PropertyKey key, String label, Composite parent) {
+		this.key = key;
+		this.label = label;
+		createControl(parent);
+	}
+
+	@Override
+	protected void fillIntoGrid(Composite parent) {
+		field = PreferencePageUtils.createSpinnerControl(parent, label);
+		field.setMinimum(7);
+		field.setMaximum(17);
+		field.setSelection(11);
+		field.setIncrement(1);
+		field.setPageIncrement(5);
+		field.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(final ModifyEvent e) {
+				valueChanged();
+			}
+		});
+	}
+
+	@Override
+	protected int getGridColumnsNumber() {
+		return 1; // one control per line in the layout grid
+	}
+
+	@Override
+	protected void load() {
+		value = getStore().load(key);
+		field.setSelection(value);
+	}
+
+	@Override
+	protected void loadDefault() {
+		value = getStore().loadDefault(key);
+		field.setSelection(value);
+	}
+
+	@Override
+	protected void save() {
+		int oldValue = getStore().load(key);
+		getStore().save(key, value);
+		getStore().firePropertyChangeEvent(key, oldValue, value, EventScope.APPLICATION);
+	}
+
+	private void valueChanged() {
+		int oldValue = value;
+		value = Integer.parseInt(field.getText());
+		getStore().firePropertyChangeEvent(key, oldValue, value, EventScope.CONFIGURATION);
+	}
+}
--- jlv-eclipse-plugin/jlv-plugin/src/com/github/rd/jlv/eclipse/ui/preferences/additional/AdditionalPreferencePage.java
+++ jlv-eclipse-plugin/jlv-plugin/src/com/github/rd/jlv/eclipse/ui/preferences/additional/AdditionalPreferencePage.java
@@ -7,6 +7,7 @@ import org.eclipse.swt.widgets.Control;
 import com.github.rd.jlv.eclipse.ui.preferences.BooleanFieldEditor;
 import com.github.rd.jlv.eclipse.ui.preferences.JlvPreferencePage;
 import com.github.rd.jlv.eclipse.ui.preferences.PreferencePageUtils;
+import com.github.rd.jlv.eclipse.ui.preferences.SpinnerFieldEditor;
 import com.github.rd.jlv.props.PropertyKey;
 
 public class AdditionalPreferencePage extends JlvPreferencePage {
@@ -21,6 +22,7 @@ public class AdditionalPreferencePage extends JlvPreferencePage {
 
 		group = PreferencePageUtils.createCompositeGroup(composite, "Loglist level color settings");
 		addField(new BooleanFieldEditor(PropertyKey.LOGLIST_LEVEL_IMAGE_KEY, "Use image to display log level", group));
+		addField(new SpinnerFieldEditor(PropertyKey.LOGLIST_FONT_SIZE_KEY, "Font size", group));
 
 		return composite;
 	}
