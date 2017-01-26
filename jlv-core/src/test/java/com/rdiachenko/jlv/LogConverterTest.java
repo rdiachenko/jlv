f391bc9a8901c4ac242c0d3a64d2e1bd1d94dfb0
Ruslan Diachenko <rd.ryly@gmail.com>
2017-01-26 06:28:41 +0000

    creating converter for logback

--- jlv-core/src/test/java/com/rdiachenko/jlv/LogConverterTest.java
+++ jlv-core/src/test/java/com/rdiachenko/jlv/LogConverterTest.java
@@ -8,12 +8,14 @@ import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.spi.LocationInfo;
-import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.ThrowableInformation;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Marker;
+
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
 
 public class LogConverterTest {
 
@@ -29,9 +31,10 @@ public class LogConverterTest {
   private static final String FILE_NAME = LogConverterTest.class.getSimpleName() + ".java";
   private static final String MARKER = "TEST";
   
+  private static final String THROWABLE_MESSAGE = "test exception";
   private static final String[] THROWABLE_INFO = {
-      "Exception in thread \"main\" java.lang.IllegalStateException: test exception",
-      "at A.main(A.java:6)"
+      "java.lang.IllegalStateException: " + THROWABLE_MESSAGE,
+      "\tat com.rdiachenko.jlv.Main.main(Main.java:12)"
   };
   private static final String THROWABLE = THROWABLE_INFO[0] + System.lineSeparator() + THROWABLE_INFO[1];
   
@@ -54,9 +57,9 @@ public class LogConverterTest {
     ThrowableInformation mockThrowableInfo = mock(ThrowableInformation.class);
     when(mockThrowableInfo.getThrowableStrRep()).thenReturn(THROWABLE_INFO);
     
-    LoggingEvent mockLoggingEvent = mock(LoggingEvent.class);
-    setFinalField(mockLoggingEvent, LoggingEvent.class.getDeclaredField("timeStamp"), NOW);
-    when(mockLoggingEvent.getLevel()).thenReturn(Level.toLevel(LEVEL));
+    org.apache.log4j.spi.LoggingEvent mockLoggingEvent = mock(org.apache.log4j.spi.LoggingEvent.class);
+    setFinalField(mockLoggingEvent, org.apache.log4j.spi.LoggingEvent.class.getDeclaredField("timeStamp"), NOW);
+    when(mockLoggingEvent.getLevel()).thenReturn(org.apache.log4j.Level.toLevel(LEVEL));
     when(mockLoggingEvent.getThreadName()).thenReturn(THREAD_NAME);
     when(mockLoggingEvent.getLoggerName()).thenReturn(LOGGER_NAME);
     when(mockLoggingEvent.getRenderedMessage()).thenReturn(MESSAGE);
@@ -78,6 +81,52 @@ public class LogConverterTest {
     Assert.assertTrue(log.getMarker().isEmpty());
     Assert.assertEquals(THROWABLE, log.getThrowable());
     Assert.assertEquals(NDC, log.getNdc());
+    Assert.assertEquals(MDC, log.getMdc());
+  }
+  
+  @Test
+  public void testLogbackEventConvertion() {
+    Marker mockMarker = mock(Marker.class);
+    when(mockMarker.getName()).thenReturn(MARKER);
+
+    StackTraceElement[] callerData = {
+        new StackTraceElement(CLASS_NAME, METHOD_NAME, FILE_NAME, Integer.parseInt(LINE_NUMBER))
+    };
+    
+    StackTraceElementProxy stackTraceElementProxy = new StackTraceElementProxy(
+        new StackTraceElement("com.rdiachenko.jlv.Main", "main", "Main.java", 12));
+    IThrowableProxy mockThrowableProxy = mock(IThrowableProxy.class);
+    when(mockThrowableProxy.getClassName()).thenReturn(IllegalStateException.class.getName());
+    when(mockThrowableProxy.getMessage()).thenReturn(THROWABLE_MESSAGE);
+    when(mockThrowableProxy.getStackTraceElementProxyArray())
+        .thenReturn(new StackTraceElementProxy[] { stackTraceElementProxy });
+
+    ch.qos.logback.classic.spi.LoggingEvent mockLoggingEvent = mock(ch.qos.logback.classic.spi.LoggingEvent.class);
+    when(mockLoggingEvent.getTimeStamp()).thenReturn(NOW);
+    when(mockLoggingEvent.getLevel()).thenReturn(ch.qos.logback.classic.Level.toLevel(LEVEL));
+    when(mockLoggingEvent.getThreadName()).thenReturn(THREAD_NAME);
+    when(mockLoggingEvent.getLoggerName()).thenReturn(LOGGER_NAME);
+    when(mockLoggingEvent.getMessage()).thenReturn(MESSAGE);
+    when(mockLoggingEvent.getFormattedMessage()).thenReturn(MESSAGE);
+    when(mockLoggingEvent.getMarker()).thenReturn(mockMarker);
+    when(mockLoggingEvent.getCallerData()).thenReturn(callerData);
+    when(mockLoggingEvent.hasCallerData()).thenReturn(true);
+    when(mockLoggingEvent.getThrowableProxy()).thenReturn(mockThrowableProxy);
+    when(mockLoggingEvent.getMDCPropertyMap()).thenReturn(MDC_MAP);
+    
+    Log log = LogConverter.convert(ch.qos.logback.classic.spi.LoggingEventVO.build(mockLoggingEvent));
+    Assert.assertEquals(DATE, log.getDate());
+    Assert.assertEquals(LEVEL, log.getLevel());
+    Assert.assertEquals(THREAD_NAME, log.getThreadName());
+    Assert.assertEquals(LOGGER_NAME, log.getLoggerName());
+    Assert.assertEquals(MESSAGE, log.getMessage());
+    Assert.assertEquals(CLASS_NAME, log.getClassName());
+    Assert.assertEquals(METHOD_NAME, log.getMethodName());
+    Assert.assertEquals(LINE_NUMBER, log.getLineNumber());
+    Assert.assertEquals(FILE_NAME, log.getFileName());
+    Assert.assertEquals(MARKER, log.getMarker());
+    Assert.assertEquals(THROWABLE, log.getThrowable());
+    Assert.assertTrue(log.getNdc().isEmpty());
     Assert.assertEquals(MDC, log.getMdc());
   }
   
