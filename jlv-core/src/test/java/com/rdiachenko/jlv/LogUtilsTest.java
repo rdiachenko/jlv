b8da8ee65c1bd98739f041fdfe350a8a68dc888b
Ruslan Diachenko <rd.ryly@gmail.com>
2017-03-04 10:46:47 +0000

    removing bad test

--- jlv-core/src/test/java/com/rdiachenko/jlv/LogUtilsTest.java
+++ jlv-core/src/test/java/com/rdiachenko/jlv/LogUtilsTest.java
@@ -10,10 +10,4 @@ public class LogUtilsTest {
     Assert.assertTrue(LogUtils.nullToEmpty(null).isEmpty());
     Assert.assertEquals("test", LogUtils.nullToEmpty("test"));
   }
-
-  @Test
-  public void formatDateTest() {
-    long timestamp = 1486505215127L;
-    Assert.assertEquals("2017-02-07 22:06:55.127", LogUtils.formatDate(timestamp));
-  }
 }
